e02ff0100618edf1a4124f00a7d3901600ef8e6f
Per Christian Wessel <per.c.wessel@gmail.com>
2023-04-24 00:10:38 +0200

    Exam Prep

--- DSL_PS_2_assignment_structure.py
+++ DSL_PS_2_assignment_structure.py
@@ -1,50 +1,92 @@
 from __future__ import print_function
 from ortools.linear_solver import pywraplp
 
+
 def main():
+    '''
+    Assignment Problem.
+    Six workers, divided in two teams, each can perform at most two tasks.
+    Team 1: 0,1,4
+    Team 2: 2,3,5
 
-  # initialize your solver, you can use the 'CLP' solver 
-  
+    Constraints:
+    1. Each worker is assigned to at most 1 task
+    2. Each task is assigned to exactly 1 worker
+    3. Worker 0 has to be assigned to either tas 2 or 3
+    '''
 
-  # initialize the cost matrix
+    # initialize your solver, you can use the 'CLP' solver
+    # CLP = CLP Linear Programming
+    solver = pywraplp.Solver.CreateSolver('CLP')
+    # initialize the cost matrix
 
+    cost = [[45, 50, 90, 80],
+            [40, 70, 55, 70],
+            [130, 100, 40, 90],
+            [45, 80, 120, 50],
+            [40, 110, 80, 95],
+            [55, 90, 70, 110]]
 
-  # declare the two teams and team_max
+    # declare the two teams and team_max
 
+    team_1 = [0, 1, 4]
+    team_2 = [2, 3, 5]
 
-  # declare num_workers and num_tasks and assign them the correct value
+    # declare num_workers and num_tasks and assign them the correct value
+    num_workers = len(cost)
+    num_tasks = len(cost[0])
 
-  x = {}
-  for i in range(num_workers):
+    x = {}
+    for i in range(num_workers):
+        for j in range(num_tasks):
+            x[i, j] = solver.IntVar(0, 1, '')
+
+    # Objective function
+    objective_terms = []
+
+    for i in range(num_workers):
+        for j in range(num_tasks):
+            objective_terms.append(cost[i][j]*x[i, j])
+
+    solver.Minimize(solver.Sum(objective_terms))
+
+    '''
+    Same as:
+    solver.Minimize(solver.Sum([cost[i][j] * x[i, j] for i in range(num_workers)
+                                for j in range(num_tasks)]))
+    '''
+
+    # Constraints
+
+    # Each worker is assigned to at most 1 task.
+
+    for i in range(num_workers):
+        solver.Add(solver.Sum([x[i, j] for j in range(num_tasks)]) <= 1)
+
+    # Each task is assigned to exactly one worker.
+
     for j in range(num_tasks):
-      x[i, j] = solver.IntVar(0, 1, '')
+        solver.Add(solver.Sum([x[i, j] for i in range(num_workers)]) == 1)
 
-  # Objective function
-  
-  # Constraints
+    # Worker 0 has to be assigned to either task 2 or 3
 
-  # Each worker is assigned to at most 1 task.
-  
-  # Each task is assigned to exactly one worker.
-  
-  # Worker 0 has to be assigned to either task 2 or 3  
-  
-  # Each team takes on two tasks.
-  
-  #solve the model
+    # Each team takes on two tasks.
+
+    # solve the model
+
+    print('Total cost = ', solver.Objective().Value())
+    print()
+    for i in range(num_workers):
+        for j in range(num_tasks):
+            if x[i, j].solution_value() > 0:
+                print('Worker %d assigned to task %d.  Cost = %d' % (
+                      i,
+                      j,
+                      cost[i][j]))
+
+    print()
+    print("Time = ", solver.WallTime(), " milliseconds")
 
 
-  print('Total cost = ', solver.Objective().Value())
-  print()
-  for i in range(num_workers):
-    for j in range(num_tasks):
-      if x[i, j].solution_value() > 0:
-        print('Worker %d assigned to task %d.  Cost = %d' % (
-              i,
-              j,
-              cost[i][j]))
-
-  print()
-  print("Time = ", solver.WallTime(), " milliseconds")
 if __name__ == '__main__':
-  main()
+    main()
--- DSL_PS_3_TSP.py
+++ DSL_PS_3_TSP.py
@@ -6,23 +6,23 @@ from ortools.constraint_solver import pywrapcp
 
 def create_data_model():
     """Stores the data for the problem."""
-    #cities: 0: Lausanne, 1: Geneva, 2: Zurich, 3: Bern, 4: Lugano, 5: Luzern, 6: Basel, 7: St. Gallen, 8: Chur
+    # cities: 0: Lausanne, 1: Geneva, 2: Zurich, 3: Bern, 4: Lugano, 5: Luzern, 6: Basel, 7: St. Gallen, 8: Chur
     data = {}
     data['distance_matrix'] = [
-       [0, 64, 229, 109, 378, 110, 201, 304, 346],
-	[64, 0, 280, 161, 370, 266, 253, 360, 398],
-	[229, 280, 0, 124, 205, 53, 86, 86, 120],
-	[109, 161, 124, 0, 277, 110, 96, 204, 241],
-	[378, 370, 205, 277, 0, 167, 262, 248, 146],
-	[110, 266, 53, 110, 167, 0, 97, 144, 142],
-	[201, 253, 86, 96, 262, 97, 0, 170, 204],
-	[304, 360, 86, 204, 248, 144, 170, 0, 104],
-	[346, 398, 120, 241, 146, 142, 204, 104, 0],
-    ] 
+        [0, 64, 229, 109, 378, 110, 201, 304, 346],
+        [64, 0, 280, 161, 370, 266, 253, 360, 398],
+        [229, 280, 0, 124, 205, 53, 86, 86, 120],
+        [109, 161, 124, 0, 277, 110, 96, 204, 241],
+        [378, 370, 205, 277, 0, 167, 262, 248, 146],
+        [110, 266, 53, 110, 167, 0, 97, 144, 142],
+        [201, 253, 86, 96, 262, 97, 0, 170, 204],
+        [304, 360, 86, 204, 248, 144, 170, 0, 104],
+        [346, 398, 120, 241, 146, 142, 204, 104, 0],
+    ]
 
-    data['num_vehicles'] = 1	
+    data['num_vehicles'] = 1
 
-    #The index of the depot, the location where all vehicles start and end their routes.
+    # The index of the depot, the location where all vehicles start and end their routes.
     data['depot'] = 0
     return data
 
@@ -37,7 +37,8 @@ def print_solution(manager, routing, solution):
         plan_output += ' {} ->'.format(manager.IndexToNode(index))
         previous_index = index
         index = solution.Value(routing.NextVar(index))
-        route_distance += routing.GetArcCostForVehicle(previous_index, index, 0)
+        route_distance += routing.GetArcCostForVehicle(
+            previous_index, index, 0)
     plan_output += ' {}\n'.format(manager.IndexToNode(index))
     print(plan_output)
     plan_output += 'Route distance: {}miles\n'.format(route_distance)
@@ -49,12 +50,16 @@ def main():
     data = create_data_model()
 
     # Create the routing index manager.
+    # The three inputs to RoutingIndexManager are:
+    # 1. Number of locations incl depot
+    # 2. Number of vehicles
+    # 3. Depot node
     manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']),
                                            data['num_vehicles'], data['depot'])
 
     # Create Routing Model.
+    # Feeding the routing model with the index_manager (=manager)
     routing = pywrapcp.RoutingModel(manager)
-
 
     def distance_callback(from_index, to_index):
         """Returns the distance between the two nodes."""
@@ -66,6 +71,7 @@ def main():
     transit_callback_index = routing.RegisterTransitCallback(distance_callback)
 
     # Define cost of each arc.
+    # Using built-in function
     routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
 
     # Setting first solution heuristic.
